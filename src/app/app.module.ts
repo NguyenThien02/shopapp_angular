5a97ef5562ea069b1e486c0c0e4c54719a6b49ff
NguyenThien02 <ngpthien02@gmail.com>
2024-10-01 22:30:41 +0700

    2 Way Data Binding trong Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { DetailProductComponent } from './detail-product/detail-product.componen
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { RegisterComponent } from './register/register.component'
          RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [RegisterComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
 
+export class RegisterComponent {
+  phone: string = '';
+  password: string = '';
+  retypePassword: string = '';
+  fullName: string = '';
+  address: string = '';
+  isAccepted: boolean = false;
+  dateOfBirth: Date;
+
+  constructor() {
+    this.phone;
+    this.password;
+    this.retypePassword;
+    this.fullName;
+    this.address;
+    this.isAccepted;
+    this.dateOfBirth = new Date();
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
+  }
+
+  onPhoneChange() {
+    console.log(`Phone typed: ${this.phone}`);
+  }
+
+  register() {
+    const message = `phone: ${this.phone}` +
+      `fullName: ${this.fullName}`;
+
+    alert(message);
+  }
 }
